a170d3ab2c2ae82f660d72a5f318b85d2e040d8b
CalebRose <ctrose17@gmail.com>
2019-12-18 17:28:22 -0600

    You're doing great

--- anything.go
+++ anything.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	fmt.Println("HELLO THERE CAN YOU PROGRAM PROBABLY NOT BECAUSE YOU NEED TO KNOW ABOUT KADANE")
+	fmt.Println("HELLO THERE CAN YOU PROGRAM PROBABLY NOT BECAUSE YOU NEED TO KNOW ABOUT KADANE ALGORITHM")
 	foo()
 
 	for i := 0; i < 100; i++ {
@@ -17,5 +17,5 @@ func main() {
 }
 
 func foo() {
-	fmt.Println("Foo foo boo hoo fuck you too")
+	fmt.Println("Foo foo boo hoo Foo Doo Bar")
 }
