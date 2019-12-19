4166e30ef52aadc61d717492a28f3aaff544c6a7
CalebRose <ctrose17@gmail.com>
2019-12-18 20:00:18 -0600

    You're doing great

---
+++ Daily_Coding_Challenge/one.go
@@ -0,0 +1,8 @@
+package dailycodingchallenge
+
+func one(list []int, target int) {
+	// Two sum
+
+	//lookup := {}
+
+}
--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 )
 
 func main() {
-	fmt.Println("HELLO THERE CAN YOU PROGRAM PROBABLY NOT BECAUSE YOU NEED TO KNOW ABOUT KADANE ALGORITHM")
+	n, _ := fmt.Println("HELLO THERE CAN YOU PROGRAM PROBABLY NOT BECAUSE YOU NEED TO KNOW ABOUT KADANE ALGORITHM")
 	foo()
+	fmt.Println(n)
 
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10; i++ {
 		if i%2 == 0 {
 			fmt.Println(i)
 		}
